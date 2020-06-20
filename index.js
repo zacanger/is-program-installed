6a3454708156de9f4b208fecab6fdd38be37af62
zacanger <zac@zacanger.com>
2020-06-20 13:20:58 -0600

    fix: handle mac

--- index.js
+++ index.js
@@ -1,8 +1,25 @@
-module.exports = (program) => {
+const { execSync } = require('child_process')
+
+const os = process.platform || require('os').platform()
+
+const exec = (cmd, opts) => execSync(cmd, opts).toString('utf8').trim()
+
+const isLinuxInstalled = (program) => {
   try {
-    require('child_process').execSync(`hash ${program} 2>/dev/null`)
+    exec(`hash ${program} 2>/dev/null`)
     return true
-  } catch {
+  } catch (_) {
     return false
   }
 }
+
+const isMacInstalled = (program) => {
+  try {
+    exec(`osascript -e 'id of application "${program}"' 2>&1>/dev/null`)
+    return true
+  } catch (_) {
+    return false
+  }
+}
+
+module.exports = os === 'darwin' ? isMacInstalled : isLinuxInstalled
